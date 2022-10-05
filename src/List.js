af9384de5dcfe2d0e9c73a7b39e9e0545f7b5005
AlmasHanif <81069710+AlmasHanif@users.noreply.github.com>
2022-10-05 14:11:23 +0500

    add emoji data

--- src/List.js
+++ src/List.js
@@ -17,7 +17,7 @@ function List(props) {
         <div className='container'>
             <div className='row mt-5 d-flex align-item-center justify-content-center'>
                 {filteredData.map((item) => (
-                    <div className='col-lg-3 col-md-4 col-sm-2 col-xs-6 emojis' key={item.id}>{item.text}</div>
+                    <div className='col-lg-3 col-md-4 col-sm-2 col-xs-6 emojis' key={item.id}>{item.name}</div>
                 ))}
             </div>
         </div>
